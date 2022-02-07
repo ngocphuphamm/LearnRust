89c233906466d8786b097c4e8b0586eea0e72603
ngocphu <ngocphupham682001@gmail.com>
2022-02-08 00:28:03 +0700

    const

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+ // CONST 
+    let mut x =  10 ; // mut = mutable có thể thay đỏi được 
+    println!("x = {}",x);
+    x=20; 
+    println!("x = {}",x);
+// KHAI BÁO BIẾN HẰNG SỐ
+// tên biến cách nhau dấu _ và ghi hoa 
+// U32 : INTERGER 32 BIT ; 
+
+// bất biến  : variable 
+//Mặc định  của biến trong rust là bất biến 
+    const HANG_SO: u32 = 1000000000000;
+    //
+    println!("HANG_SO = {}",HANG_SO);
+
+
+
+    
 }
